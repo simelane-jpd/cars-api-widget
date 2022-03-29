5cf2fb2060a24065099db6e06f8d38f63de670f1
simelane-jpd <201575227@student.uj.ac.za>
2022-03-29 15:25:15 +0200

    added cars

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-//const cars = document.querySelector('.cars');
+const cars = document.querySelector('.cars');
 const colors = document.querySelector('.colors');
 const carbrands = document.querySelector('.carbrands');
 
 
-//axios
-    //.get("https://api-tutor.herokuapp.com/v1/cars")
-    //.then(function (result) {
-        //result.data.forEach(car => {
-           // const li = document.createElement('tr');
-           // li.innerHTML = `<tr>
-        //<td>${car.reg_number}<strong></strong></td></tr>`
-           // cars.appendChild(li);
+axios
+    .get("https://api-tutor.herokuapp.com/v1/cars")
+    .then(function (result) {
+        result.data.forEach(car => {
+            const li = document.createElement('tr');
+            li.innerHTML = `<tr>
+        <td>${car.make},
+        
+        ${car.model},
+        ${car.color},
+        ${car.price},
+        ${car.reg_number}</td></tr>`
+            cars.appendChild(li);
 
-        //});
+        });
 
-    //});
+    });
 
     axios
     .get("https://api-tutor.herokuapp.com/v1/colors")
