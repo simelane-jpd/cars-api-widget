d45dd0c15d0c680e4559b5332ab0e3a39bdc2394
simelane-jpd <201575227@student.uj.ac.za>
2022-03-29 13:56:15 +0200

    added car models

---
+++ index.js
@@ -0,0 +1,29 @@
+const cars = document.querySelector('.cars');
+const colours = document.querySelector('.colours');
+
+
+axios
+    .get("http://api-tutor.herokuapp.com/v1/cars")
+    .then(function (result) {
+        result.data.forEach(car => {
+            const li = document.createElement('tr');
+            li.innerHTML = `<tr>
+        <td>${car.model}<strong></strong></td></tr>`
+            cars.appendChild(li);
+
+        });
+
+    });
+
+    //axios
+    //.get("http://api-tutor.herokuapp.com/v1/colors")
+    //.then(function (result) {
+    //    result.data.forEach(car => {
+            //const li = document.createElement('tr');
+           // li.innerHTML = `<tr>
+       // <td>${car}<strong></strong></td></tr>`
+          //  cars.appendChild(li);
+
+       // });
+
+    //});
